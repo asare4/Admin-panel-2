2696e3956cf3549d508ac00bcf4fb6ca0a682603
Asare <asare.ee4@gmail.com.github.com>
2025-09-07 11:48:53 +0000

    feat(navbar): add navigation bar to admin panel

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ allSideMenu.forEach(item=> {
         li.classList.add('active');
     })
 });
+
+
+
+
+// TOGGLE SIDEBAR 
+const menuBar = document.querySelector('#content nav .bxr.bxs-menu');
+const sideBar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+    sideBar.classList.toggle('hide');
+});
+
+
+
+
+if(window.innerWidth < 768) {
+    sideBar.classList.add('hide');
+} else if(window.innerWidth < 576) {
+    const searchButton = document.querySelector('#content nav form .form-input button');
+    const searchForm = dpcument.querySelector('#content nav form');
+
+    searchButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        searchForm.classList.toggle('show');
+    })
+}
