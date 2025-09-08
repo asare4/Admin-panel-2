e6a92b8d466c6393357d333ddbdac748825e570c
Asare <asare.ee4@gmail.com.github.com>
2025-09-08 03:51:58 +0000

    feat(navbar): add navbar to admin panel

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ allSideMenu.forEach(item=> {
 
 
 // TOGGLE SIDEBAR 
-const menuBar = document.querySelector('#content nav .bxr.bxs-menu');
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
 const sideBar = document.getElementById('sidebar');
 
 menuBar.addEventListener('click', function () {
@@ -23,16 +23,41 @@ menuBar.addEventListener('click', function () {
 });
 
 
+ 
+
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+    if(window.innerWidth < 576) {
+        e.preventDefault();
+        searchForm.classList.toggle('show');
+        if(searchForm.classList.contains('show')) {
+            searchButtonIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+})
+
+
 
 
 if(window.innerWidth < 768) {
     sideBar.classList.add('hide');
-} else if(window.innerWidth < 576) {
-    const searchButton = document.querySelector('#content nav form .form-input button');
-    const searchForm = dpcument.querySelector('#content nav form');
-
-    searchButton.addEventListener('click', function (e) {
-        e.preventDefault();
-        searchForm.classList.toggle('show');
-    })
+} else if (window.innerWidth > 576) {
+    searchButtonIcon.classList.replace('bx-x', 'bx-search');
+    searchForm.classList.remove('show');
 }
+
+
+
+window.addEventListener('resize', function () {
+    if (this.innerWidth > 576) {
+        searchButtonIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
+})
